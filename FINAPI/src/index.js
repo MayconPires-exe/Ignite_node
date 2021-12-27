d9b1add3f0e18c0049cdfd1e5fdaf57c2cff08de
Maycon Pires <piresmaycon4@gmail.com>
2021-12-27 20:20:18 -0300

    listagem de extrato bancário por data

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -108,5 +108,21 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send();
 });
 
+// lista de extrato bancário
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) => 
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  )
+
+  return response.json(statement);
+});
+
 // localhost:
 app.listen(3333);
