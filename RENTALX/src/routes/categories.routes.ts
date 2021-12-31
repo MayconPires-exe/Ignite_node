29bdc1b7dc1dff7730884e6783c29c95035c75fd
Maycon Pires <piresmaycon4@gmail.com>
2021-12-31 19:34:27 -0300

    Validação de cadastro de categoria

--- RENTALX/src/routes/categories.routes.ts
+++ RENTALX/src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { request, Router } from 'express';
+import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
@@ -6,6 +6,12 @@ const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exists!" })
+  }
 
   categoriesRepository.create({ name, description });
 
@@ -16,6 +22,6 @@ categoriesRoutes.get("/", (request, response) => {
   const all = categoriesRepository.list();
 
   return response.json(all);
-})
+});
 
 export { categoriesRoutes };
