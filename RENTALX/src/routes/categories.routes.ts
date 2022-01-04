5b9994575531d448f08ca1af3d9f082b7ed609b1
Maycon Pires <piresmaycon4@gmail.com>
2022-01-03 23:51:27 -0300

    Utilização do princípio da substituição de Liskov LPS

--- RENTALX/src/routes/categories.routes.ts
+++ RENTALX/src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+
 import { CreateCategoryService } from '../services/CreateCategoryService';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
