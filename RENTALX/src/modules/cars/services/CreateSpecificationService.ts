4f59358e3c4afba2334f426855bc73a537e32857
Maycon da Silva Pires <piresmaycon4@gmail.com>
2022-02-17 17:17:27 -0300

    Criando repositório de especificação

--- RENTALX/src/modules/cars/services/CreateSpecificationService.ts
+++ RENTALX/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,9 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
 
+interface IRequest {
+  name: string;
+  description: string;
+}
 
 class CreateSpecificationService {
+  constructor(private SpecificationsRepository: ISpecificationsRepository) {
+  }
 
-  execute() {
-    console.log("TODO");
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.SpecificationsRepository.findByName(name);
+
+    if(specificationAlreadyExists) {
+      throw new Error("Specification already exists!")
+    }
+
+    this.SpecificationsRepository.create({
+      name,
+      description,
+    })
   }
 }
 
